62e5fdcd4c81b37bab6cf16192ede3e8d8e2e6e3
lim2617 <sl_darkeos@hotmail.com>
2021-08-19 01:33:56 +0800

    Added Javascript
    But not everything successful

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
-/**
- * Try to manipulate the page
- * 
- * Easiest
- * 1) Get the title for each dog/cat services, and put them into the console using console.log
- * 2) Change the title of the page to "We're the CAT People"
- * 3) Change the main image to be something cat related.
- * 
- * 
- * Harder
- * 1) Replace the call to action button background color with "red"
- * 2) Find another image and replace the header (try unsplash.com, get familiar with searching for images)
- * 3) When someone clicks the "Book your next Rover sitter" button, change the text of the button to say "Thanks"
- * 
- * 
- * Hardest (only if you get the others first)
- * 1) Make the responsive menu work!
- * 2) Implement a modal window - "Sign up now" CTA when someone clicks the 'Sign Up' button
- */ 
+// let heading = document.querySelector("h1");
+// heading.innerHTML = "We're The CAT People";
+
+// let image = document.querySelector("#image");
+// image.src = "https://cdn.builtin.com/sites/www.builtin.com/files/styles/og/public/Rover%2520Cat_4_0.jpg";
+
+// let action = document.querySelector(".signIn");
+// action.style.backgroundColor = "pink";
+
+
+// let btn = document.querySelector("#button");
+// btn.onclick = function(){
+//     btn.innerHTML = "THANKS!"
+// }
+
+// let modal = document.querySelector(".modal");
+// let signin = document.querySelector("#signin");
+
+// signin.onclick = function(){
+//     modal.style.display = "block"
+// }
+
+// window.onclick = function(e){
+//     if (e.target == modal){
+//         modal.style.display = "none";
+//     }
+// }
